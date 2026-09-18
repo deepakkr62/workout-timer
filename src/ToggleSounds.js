0e89548ffa64d362d1fa02f8eb7a671dec965561
deepakkr62 <deepakkr11113@gmail.com>
2026-09-18 10:41:34 +0530

    fix performance

--- src/ToggleSounds.js
+++ src/ToggleSounds.js
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 function ToggleSounds({ allowSound, setAllowSound }) {
   return (
     <button
@@ -9,4 +11,5 @@ function ToggleSounds({ allowSound, setAllowSound }) {
   );
 }
 
-export default ToggleSounds;
+export default memo(ToggleSounds);
+//this is another way to memoized component
